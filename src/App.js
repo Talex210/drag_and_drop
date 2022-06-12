f4355427bc8bb6e6a0d920445bb8c54cc0d3d25b
TALEX <tal.89@mail.ru>
2022-06-12 19:56:18 +0300

    Drag and drop на React. Аналог TRELLO. Доска задач

--- src/App.js
+++ src/App.js
@@ -2,62 +2,106 @@ import './App.css';
 import {useState} from 'react';
 
 function App() {
-    const [cardList, setCardList] = useState([
-        {id: 1, order: 1, text: 'Карточка 1'},
-        {id: 2, order: 2, text: 'Карточка 2'},
-        {id: 3, order: 3, text: 'Карточка 3'},
-        {id: 4, order: 4, text: 'Карточка 4'},
+
+    const [boards, setBoards] = useState([
+        {
+            id: 1,
+            title: 'Сделать',
+            items: [{id: 1, title: 'Пойти в магазин'}, {id: 2, title: 'Выкенуть мусор'}, {id: 3, title: 'Покушать'}]
+        },
+        {
+            id: 2,
+            title: 'Проверить',
+            items: [{id: 4, title: 'Код ревью'}, {id: 5, title: 'Задачи на факториал'}, {
+                id: 6, title: 'Задачи на Фибоначи'
+            }]
+        },
+        {
+            id: 3,
+            title: 'Сделано',
+            items: [{id: 7, title: 'Снять видео'}, {id: 8, title: 'Смонтировать'}, {id: 9, title: 'Отрендерить'}]
+        }
     ])
 
-    const [currentCard, setCurrentCard] = useState(null)
-
-    function dragStartHandler(e, card) {
-        console.log('drag', card)
-        setCurrentCard(card)
-    }
-
-    function dragEndHandler(e) {
-        e.target.style.background = 'black';
-    }
+    const [currentBoard, setCurrentBoard] = useState(null)
+    const [currentItem, setCurrentItem] = useState(null)
 
     function dragOverHandler(e) {
         e.preventDefault()
-        e.target.style.background = 'lightgray';
+        if (e.target.className === 'item') {
+            e.target.style.boxShadow = '0 4px 3px white'
+        }
     }
 
-    function dropHandler(e, card) {
+    function dragLeaveHandler(e) {
+        e.target.style.boxShadow = 'none'
+    }
+
+    function dragStartHandler(e, board, item) {
+        setCurrentBoard(board);
+        setCurrentItem(item);
+    }
+
+    function dragEndHandler(e) {
+        e.target.style.boxShadow = 'none'
+    }
+
+    function dropHandler(e, board, item) {
         e.preventDefault()
-        console.log('drop', card)
-        setCardList(cardList.map(c => {
-            if (c.id === card.id) {
-                return {...c, order: currentCard.order}
+        e.stopPropagation()
+        const currentIndex = currentBoard.items.indexOf(currentItem);
+        currentBoard.items.splice(currentIndex, 1);
+        const dropIndex = board.items.indexOf(item);
+        board.items.splice(dropIndex + 1, 0, currentItem);
+        setBoards(boards.map(b => {
+            if (b.id === board.id) {
+                return board;
             }
-            if (c.id === currentCard.id) {
-                return {...c, order: card.order}
+            if (b.id === currentBoard.id) {
+                return currentBoard;
             }
-            return c;
+            return b;
         }))
-        e.target.style.background = 'black';
+        e.target.style.boxShadow = 'none'
     }
 
-    const sortCards = (a, b) => {
-        if (a.order > b.order) {
-            return 1
-        } else { return -1}
+    function dropCardHandler(e, board) {
+        // e.stopPropagation()
+        board.items.push(currentItem);
+        const currentIndex = currentBoard.items.indexOf(currentItem);
+        currentBoard.items.splice(currentIndex, 1);
+        setBoards(boards.map(b => {
+            if (b.id === board.id) {
+                return board;
+            }
+            if (b.id === currentBoard.id) {
+                return currentBoard;
+            }
+            return b;
+        }))
+        e.target.style.boxShadow = 'none'
     }
 
     return (
         <div className='App'>
-            {cardList.sort(sortCards).map(card =>
-                <div className='card'
-                     key={card.id}
-                     draggable={true}
-                     onDragStart={(e) => dragStartHandler(e, card)}
-                     onDragLeave={(e) => dragEndHandler(e)}
-                     onDragEnd={(e) => dragEndHandler(e)}
-                     onDragOver={(e) => dragOverHandler(e)}
-                     onDrop={(e) => dropHandler(e, card)}>
-                    {card.text}
+            {boards.map(board =>
+                <div className='board'
+                     key={board.id}
+                     onDragOver={e => dragOverHandler(e)}
+                     onDrop={e => dropCardHandler(e, board)}>
+                    <div className='board__title'>{board.title}</div>
+                    {board.items.map(item =>
+                        <div className='item'
+                             key={item.id}
+                             draggable={true}
+                             onDragOver={e => dragOverHandler(e)}
+                             onDragLeave={e => dragLeaveHandler(e)}
+                             onDragStart={e => dragStartHandler(e, board, item)}
+                             onDragEnd={e => dragEndHandler(e)}
+                             onDrop={e => dropHandler(e, board, item)}>
+                            {item.title}
+                        </div>
+                    )}
                 </div>
             )}
         </div>
